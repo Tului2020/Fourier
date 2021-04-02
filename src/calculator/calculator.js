086dcd86c237910d02d0565eaae1911f9a5c4486
Tului <tgantulga@ucdavis.edu>
2021-04-02 12:37:27 -0700

    more good changes

--- src/calculator/calculator.js
+++ src/calculator/calculator.js
@@ -7,32 +7,44 @@ import Algebrite from 'algebrite'
 class Calculator extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { equation: '' }
+    this.state = { equation: 'x', lowerLimit: -5, upperLimit: 5 }
     this.updateEquation = this.updateEquation.bind(this)
+    this.updateState = this.updateState.bind(this)
   }
 
   render() {
     return (
       <div>
         <div id='result'>No Equation</div>
-        <input placeholder={'x + x'} className='equation' onChange={this.updateEquation} />
-        <div>
-          <input placeholder={'Lower Limit'} className='equation' />
-          <input placeholder={'Upper Limit'} className='equation' />
+        <input value={this.state.equation} className='equations' id='equation' onChange={this.updateState}/>
+        <div className='limits'>
+          <div id='lower-limit'>
+            Lower Limit <input value={this.state.lowerLimit} className='equation' id='lowerLimit' onChange={this.updateState}/>
+          </div>
+          <div id='upper-limit'>
+            Upper Limit <input value={this.state.upperLimit} className='equation' id='lowerLimit'onChange={this.updateState}/>
+          </div>
         </div>
+        <button onClick={this.updateEquation}>Update</button>
       </div>
     )
   }
 
-  updateEquation(event) {
-    let equation = event.target.value
-    this.setState({ equation })
+  updateState(event) {
+    let stateToChange = event.target.id
+    let newValue = event.target.value
+    this.setState({[stateToChange]: newValue})
+  }
 
+
+  updateEquation() {
+    let equation = document.getElementById('equation').value
     let resultElement = document.getElementById('result')
-    if (!event.target.value) {
+
+    if (!equation) {
       resultElement.innerHTML = 'No Equation'
     } else {
-      resultElement.innerHTML = Algebrite.run(equation)
+      resultElement.innerHTML = Algebrite.defint(Algebrite.run(equation), 'x', 0, 10)
     }
   }
 
